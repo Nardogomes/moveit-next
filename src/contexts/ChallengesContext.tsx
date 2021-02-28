681511b948e55b344f23f6ee9a24467c54053a04
Ednardo Gomes <ednardo@email.com>
2021-02-28 18:32:49 -0300

    Criando o componente LevelUpModal.tsx e seu arquivo de estilização LevelUpModal.module.css.

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState, ReactNode, useEffect } from 'react'
 import Cookies from 'js-cookie'
 import challenges from '../../challenges.json'
+import { LevelUpModal } from '../components/LevelUpModal'
 
 interface Challenge {
   type: 'body' | 'eye',
@@ -105,6 +106,8 @@ export function ChallengesProvider({ children, ...rest }: ChallengesProviderProp
       }}
     >
       {children}
+
+      <LevelUpModal />
     </ChallengesContext.Provider>
   )
 }
